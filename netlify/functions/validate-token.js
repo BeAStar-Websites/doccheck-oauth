5a4e4b8088764a69321bf7ee125efc9a7016eb67
BeAStar-Websites <farzan@be-a-star-productions.de>
2025-02-27 11:50:08 +0100

    Update validate-token.js

--- netlify/functions/validate-token.js
+++ netlify/functions/validate-token.js
@@ -2,9 +2,14 @@ exports.handler = async (event, context) => {
   const cookieHeader = event.headers.cookie || '';
   // Überprüfe, ob der "token" Cookie existiert
   const isValid = cookieHeader.includes('token=');
-  
+
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://www.420pharma.de", // oder "*" für alle, aber besser auf deine Domain beschränken
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ valid: isValid }),
   };
 };
